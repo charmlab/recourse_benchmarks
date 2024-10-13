a0d0825eecadf3b03b7747d309a4aef6fbb8c123
MohammadReza Alavi <102237124+mralavi20@users.noreply.github.com>
2024-10-13 22:39:51 +0330

    update reproduce.py file
    The test added measures the individual cost of applying recourse, specifically focusing on the Euclidean distance between the original factual instances and the counterfactuals

--- recourse_methods/catalog/claproar/reproduce.py
+++ recourse_methods/catalog/claproar/reproduce.py
@@ -65,6 +65,32 @@ def test_claproar_distribution_shift(dataset_name):
     assert abs(mmd_value - expected_mmd_value) <= tolerance or abs(expected_mmd_value - mmd_value) <= tolerance, \
         f"MMD value mismatch."
 
+@pytest.mark.parametrize("dataset_name", [
+    ("credit")
+])
+def test_claproar_individual_cost(dataset_name):
+    data = DataCatalog(dataset_name, "linear", 0.7)
+    model = ModelCatalog(data, "linear", backend="pytorch")
+
+    claproar = ClaPROAR(mlmodel=model)
+
+    total_factuals = predict_negative_instances(model, data)
+
+    factuals = total_factuals.iloc[:5]
+
+    counterfactuals = claproar.get_counterfactuals(factuals)
+
+    negative_instances = predict_negative_instances(model, data).iloc[:5]
+
+    factuals_np = negative_instances.drop("y", axis=1).to_numpy()
+    counterfactuals_np = counterfactuals.to_numpy()
+    individual_cost = np.linalg.norm(counterfactuals_np - factuals_np, axis=1).mean()
+
+    expected_cost = 0.5
+
+    assert individual_cost <= expected_cost, \
+        f"Individual cost too high."
+
 def compute_mmd(X, Y, gamma=0.05):
         K_XX = rbf_kernel(X, X, gamma=gamma)
         K_YY = rbf_kernel(Y, Y, gamma=gamma)
