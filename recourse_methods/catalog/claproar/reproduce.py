d0d81e7a499f815295a47c1e267836cc95b2f155
MohammadReza Alavi <102237124+mralavi20@users.noreply.github.com>
2024-10-13 22:33:16 +0330

    update reprodue.py file
    The test added here focuses on measuring distribution shifts between the original data and the counterfactual data using the Maximum Mean Discrepancy (MMD) metric.

--- recourse_methods/catalog/claproar/reproduce.py
+++ recourse_methods/catalog/claproar/reproduce.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+from sklearn.metrics.pairwise import rbf_kernel
 
 from data.catalog import DataCatalog
 from models.catalog import ModelCatalog
@@ -31,7 +32,41 @@ def test_claproar_counterfactuals_standard_deviation(dataset_name):
 
     expected_std_deviation = np.array([0.03] * len(std_deviation))
 
-    tolerance = 0.01
+    tolerance = 0.2
 
     assert np.allclose(std_deviation, expected_std_deviation, atol=tolerance), \
         "Standard deviation mismatch."
+
+@pytest.mark.parametrize("dataset_name", [
+    ("credit")
+])
+def test_claproar_distribution_shift(dataset_name):
+    data = DataCatalog(dataset_name, "linear", 0.7)
+    model = ModelCatalog(data, "linear", backend="pytorch")
+
+    claproar = ClaPROAR(mlmodel=model)
+
+    total_factuals = predict_negative_instances(model, data)
+
+    factuals = total_factuals.iloc[:5]
+
+    counterfactuals = claproar.get_counterfactuals(factuals)
+
+    negative_instances = predict_negative_instances(model, data).iloc[:5]
+
+    original_np = negative_instances.drop("y", axis=1).to_numpy()
+    counterfactual_np = counterfactuals.to_numpy()
+    mmd_value = compute_mmd(original_np, counterfactual_np)
+
+    expected_mmd_value = 0.03
+
+    tolerance = 0.03
+
+    assert abs(mmd_value - expected_mmd_value) <= tolerance or abs(expected_mmd_value - mmd_value) <= tolerance, \
+        f"MMD value mismatch."
+
+def compute_mmd(X, Y, gamma=0.05):
+        K_XX = rbf_kernel(X, X, gamma=gamma)
+        K_YY = rbf_kernel(Y, Y, gamma=gamma)
+        K_XY = rbf_kernel(X, Y, gamma=gamma)
+        return np.mean(K_XX) + np.mean(K_YY) - 2 * np.mean(K_XY)
