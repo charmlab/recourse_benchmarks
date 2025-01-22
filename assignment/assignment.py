1abcc892036c06a4395d97516df301dd01429607
Zahra Khotanlou <84021970+zkhotanlou@users.noreply.github.com>
2025-01-22 03:23:04 -0500

    fix: tools impacted by structure changes

--- assignment/assignment.py
+++ assignment/assignment.py
@@ -25,7 +25,7 @@ from methods import Dice  # TO-DO: Replace with implemented recourse method
 from methods.utils import create_hash_dataframe
 from models.catalog import ModelCatalog
 from models.predict_factuals import predict_negative_instances
-from tools.logging import log
+from tools.logging_tools import log
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 warnings.simplefilter(action="ignore", category=FutureWarning)
