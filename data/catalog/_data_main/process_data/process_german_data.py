45ae92fded11fea91c24a57125e0959113604cd4
Hashir <hashirahmed204@gmail.com>
2025-10-25 10:32:55 -0400

    reduced duplication in loading german data

--- data/catalog/_data_main/process_data/process_german_data.py
+++ data/catalog/_data_main/process_data/process_german_data.py
@@ -13,66 +13,35 @@ seed(
 np.random.seed(RANDOM_SEED)
 
 
-def load_german_data():
+def load_german_data(modified=False):
     # input vars
-    raw_data_file = os.path.join(os.path.dirname(__file__), "german_raw.csv")
-    processed_file = os.path.join(os.path.dirname(__file__), "german_processed.csv")
+    if modified == False:
+        raw_data_file = os.path.join(os.path.dirname(__file__), "german_raw.csv")
+        processed_file = os.path.join(os.path.dirname(__file__), "german_processed.csv")
+    else:
+        raw_data_file = os.path.join(os.path.dirname(__file__), "german_v2.csv")
+        processed_file = os.path.join(os.path.dirname(__file__), "german_v2_processed.csv")
 
     # German Data Processing
     raw_df = pd.read_csv(raw_data_file)  # , index_col = 0)
     processed_df = pd.DataFrame()
 
-    processed_df["GoodCustomer (label)"] = raw_df["GoodCustomer"]
-    processed_df["GoodCustomer (label)"] = (
-        processed_df["GoodCustomer (label)"] + 1
-    ) / 2
-    processed_df.loc[raw_df["Gender"] == "Male", "Sex"] = 1
-    processed_df.loc[raw_df["Gender"] == "Female", "Sex"] = 0
-    processed_df["Age"] = raw_df["Age"]
-    processed_df["Credit"] = raw_df["Credit"]
-    processed_df["LoanDuration"] = raw_df["LoanDuration"]
-
-    # # order important, more balance can overwrite less balance!
-    # processed_df.loc[raw_df['CheckingAccountBalance_geq_0'] == 1, 'CheckingAccountBalance'] = 2
-    # processed_df.loc[raw_df['CheckingAccountBalance_geq_200'] == 1, 'CheckingAccountBalance'] = 3
-    # processed_df = processed_df.fillna(1) # all other categories...
-
-    # # order important, more balance can overwrite less balance!
-    # processed_df.loc[raw_df['SavingsAccountBalance_geq_100'] == 1, 'SavingsAccountBalance'] = 2
-    # processed_df.loc[raw_df['SavingsAccountBalance_geq_500'] == 1, 'SavingsAccountBalance'] = 3
-    # processed_df = processed_df.fillna(1) # all other categories...
-
-    # # 2: owns house, 1: rents house, 0: neither
-    # processed_df.loc[raw_df['OwnsHouse'] == 1, 'HousingStatus'] = 3
-    # processed_df.loc[raw_df['RentsHouse'] == 1, 'HousingStatus'] = 2
-    # processed_df = processed_df.fillna(1) # all other categories...
-
-    # Save to CSV
-    processed_df = processed_df + 0  # convert boolean values to numeric
-    processed_df = processed_df.reset_index(drop=True)
-    processed_df = (
-        processed_df.dropna()
-    )  # drop all rows that include NAN (some exist in isMarried column, possibly elsewhere as well)
-    processed_df.to_csv(processed_file, header=True, index=False)
-    assert processed_df.shape[0] == 1000
-
-    return processed_df.astype("float64")
-
-
-def load_german_data_modified():
-    # input vars
-    raw_data_file = os.path.join(os.path.dirname(__file__), "corrected_german.csv")
-    processed_file = os.path.join(os.path.dirname(__file__), "corrected_german_processed.csv")
-
-    # German Data Processing
-    raw_df = pd.read_csv(raw_data_file)  # , index_col = 0)
-    processed_df = pd.DataFrame()
-
-    processed_df["GoodCustomer (label)"] = raw_df["credit_risk"]
-    processed_df["Sex"] = raw_df["personal_status_sex"]
-    processed_df["Age"] = raw_df["age"]
-    processed_df["Credit"] = raw_df["amount"]
-    processed_df["LoanDuration"] = raw_df["duration"]
+    if modified == False:
+        processed_df["GoodCustomer (label)"] = raw_df["GoodCustomer"]
+        processed_df["GoodCustomer (label)"] = (
+            processed_df["GoodCustomer (label)"] + 1
+        ) / 2
+        processed_df.loc[raw_df["Gender"] == "Male", "Sex"] = 1
+        processed_df.loc[raw_df["Gender"] == "Female", "Sex"] = 0
+        processed_df["Age"] = raw_df["Age"]
+        processed_df["Credit"] = raw_df["Credit"]
+        processed_df["LoanDuration"] = raw_df["LoanDuration"]
+    else:
+        processed_df["GoodCustomer (label)"] = raw_df["credit_risk"]
+        processed_df["Sex"] = raw_df["personal_status_sex"]
+        processed_df["Age"] = raw_df["age"]
+        processed_df["Credit"] = raw_df["amount"]
+        processed_df["LoanDuration"] = raw_df["duration"]
 
     # # order important, more balance can overwrite less balance!
     # processed_df.loc[raw_df['CheckingAccountBalance_geq_0'] == 1, 'CheckingAccountBalance'] = 2
