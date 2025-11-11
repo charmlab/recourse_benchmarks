e5b9d2b04b3ec0314e069ca9452dc825817f3385
Jamie001129 <Jamie001129@yujieming@outlook.com>
2025-11-10 23:20:44 -0500

    Start to fix the NICE implementation: use the author's data and model

---
+++ methods/catalog/nice/nice_experiments/__init__.py
@@ -0,0 +1,8 @@
+from .pmlb_fetcher import PmlbFetcher
+from .adapter import NICEExperimentsDataAdapter, NICEExperimentsModelAdapter
+
+__all__ = [
+    'PmlbFetcher',
+    'NICEExperimentsDataAdapter', 
+    'NICEExperimentsModelAdapter'
+]
