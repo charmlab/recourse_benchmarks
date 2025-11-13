e95b00bf654e4f94ebb55cce829ff4c425bcb777
Jamie001129 <Jamie001129@yujieming@outlook.com>
2025-11-12 22:39:33 -0500

    update test auc

--- methods/catalog/nice/nice_experiments/tune_rf.py
+++ methods/catalog/nice/nice_experiments/tune_rf.py
@@ -6,6 +6,7 @@ from sklearn.metrics import roc_auc_score
 
 from pmlb_fetcher import PmlbFetcher
 from adapter import NICEExperimentsDataAdapter, NICEExperimentsModelAdapter
+from preprocessing import OHE_minmax
 
 fetcher = PmlbFetcher('adult', test_size=0.2, explain_n=200)
 nice_data = fetcher.dataset
@@ -15,6 +16,18 @@ data_adapted = NICEExperimentsDataAdapter(nice_data)
 # Train model using NICE_experiments data
 X_train = data_adapted.df_train.drop(columns=['y']).values
 y_train = data_adapted.df_train['y'].values
+X_test = data_adapted.df_test.drop(columns=['y']).values
+y_test = data_adapted.df_test['y'].values
+
+preprocessor = OHE_minmax(                           
+        cat_feat=nice_data['cat_feat'],                  
+        con_feat=nice_data['con_feat']                   
+    ) 
+preprocessor.fit(nice_data['X_train'])
+
+
+X_train_pp = preprocessor.transform(X_train) 
+X_test_pp = preprocessor.transform(X_test)
 
 model = RandomForestClassifier(random_state=42)
 
@@ -33,18 +46,17 @@ grid_search = GridSearchCV(
     verbose=2
 )
 
-grid_search.fit(X_train, y_train)
+grid_search.fit(X_train_pp, y_train)
 
 print("\nBest parameters:", grid_search.best_params_)
 print("Best CV score:", grid_search.best_score_)
 
 model_best = grid_search.best_estimator_
-X_test = data_adapted.df_test.drop(columns=['y']).values
-y_test = data_adapted.df_test['y'].values
-test_acc = model_best.score(X_test, y_test)
-y_pred_proba = model_best.predict_proba(X_test)[:, 1]  # Get probability for class 1
+
+test_acc = model_best.score(X_test_pp, y_test)
+y_pred_proba = model_best.predict_proba(X_test_pp)[:, 1]  # Get probability for class 1
 test_auc = roc_auc_score(y_test, y_pred_proba)
 print(f"Test accuracy: {test_acc:.4f}")
 print(f"Test AUC: {test_auc:.4f}")
 
-# note: Test AUC: 0.9168
+# note: Test AUC: 0.9173
